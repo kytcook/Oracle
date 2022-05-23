a63dd4d6b5627fd6c89ad7988ce1a73c4f5a8f18
DESKTOP-G09GV65\user1 <kytcook@naver.com>
2022-05-23 15:07:41 +0900

    20220523_1507

---
+++ Toad/day_/20220523/20220523_orc11_1.sql
@@ -0,0 +1,38 @@
+SELECT 1+5, 6+2, 2*3, 10/2 FROM dual --옵티마이저를 통한 연산
+-- ,(콤마)를 쓸 때 마다 오른쪽으로 길어진다
+-- 전체적인 패턴을 잘 볼 것.
+-- SELECT와 FROM 절 안에는1.COLUMN이 온다
+--                    2.함수가 온다
+-- 여러개가 올 수 있다. 연결절은 ,(콤마)
+SELECT count(empno) FROM emp
+
+SELECT count(empno) 
+  FROM (SELECT empno FROM emp WHERE sal >= 2000) - 인라인뷰 - 보안 - 금융권
+--FROM 절 뒤에 집합이 온다.
+
+--DBA 
+  
+SELECT empno FROM emp WHERE sal >= 2000
+
+SELECT 1 FROM DUAL
+UNION ALL
+SELECT 2 FROM DUAL
+UNION ALL
+SELECT 3 FROM DUAL
+-- 패턴학습 : 세로와 가로에 대한 이해/ 집합적인 사고
+
+SELECT deptno FROM emp
+MINUS
+SELECT deptno FROM dept
+
+
+SELECT deptno FROM dept
+MINUS
+SELECT deptno FROM emp
+
+SELECT deptno FROM dept
+INTERSECT
+SELECT deptno FROM emp
+-- 부서 4가지 사원 3가지 / 부서가 높은 집함
+
+DELETE FROM dept --집함에 있는 데이터를 삭제한다.
---
+++ Toad/day_/20220523/20220523_orc11_2.sql
@@ -0,0 +1,79 @@
+TEMP의 자료 중에서 HOBBY가 등산 또는 낚시 인 경우를 읽어오기
+위하여 다음과 같이 조건을 줄 수 있다.
+?
+SELECT
+       emp_id, emp_name, hobby
+  FROM temp
+ WHERE hobby IN ('등산', '낚시'); -- or이니까 둘 중에 하나만 만족해도 된다.
+ 
+?
+HOBBY가 NULL 또는 '등산'인 경우
+?
+SELECT 
+       emp_id, emp_name, hobby
+  FROM temp
+ WHERE hobby IN (NULL,'등산') -- 모른다를 비교할 순 없다.
+?
+SELECT 100 + null, 5*2, 5*null --NULL은 안된다. 암튼 안된다.
+  FROM dual 
+  
+  
+SELECT 
+       comm
+  FROM emp
+ORDER BY comm asc -- 오름차순 점점 크게
+
+SELECT 
+       comm
+  FROM emp
+ORDER BY comm asc -- 오름차순 점점 크게
+
+--NULL은 어떤 값에 연산해도 NULL이므로 값을 모른다. FALSE이다.
+
+HOBBY가 NULL인 경우에 해당하는 6건은 빠져있다
+왜 빠졌을까?
+?
+위 조건을 다시 쓰면 다음과 같다.
+WHERE hobby = NULL --자체가 항상 false이다.
+   OR hobby = '등산'
+   
+이렇게 되면 hobby = NULL 이란 조건은 항상 FALSE가 RETURN된다.
+왜일까?
+NULL은 항상 IS NULL 또는 IS NOT NULL로 비교되어야 하기 때문이다.
+-- is null과 is not not습관화
+?
+따라서 hobby = '등산' 인 경우만이 검색된다.
+?
+그나마 IN이 OR의 개념과 같으므로 hobby = '등산'인 경우라도 
+검색된 것이다.
+?
+그렇다면 
+hobby가 NULL 또는 '낚시' 모두에 속하지 않는 경우
+?
+SELECT 
+       emp_id, emp_name, hobby
+  FROM temp
+ WHERE hobby NOT IN (NULL,'낚시');   
+?
+이 경우 NULL과 '낚시'를 제외한 나머지 경우인 3건을 모두 검색할까
+아니면 낚시인 경우만 제외한 9건을 검색할까?
+?
+둘 다 아니다 .
+한건도 검색하지 못한다.
+?
+NOT IN연산자에 NULL이 포함되면 어떠한 경우 일지라도 한 건도
+DATA를 검색하지 못한다.
+?
+SELECT * FROM temp
+ WHERE hobby <> NULL;
+ 
+SELECT * FROM temp
+ WHERE hobby <> NULL
+   AND hobby <> '낚시';
+ 
+SELECT * FROM temp
+ WHERE hobby IS NOT NULL; 
+  
+SELECT * FROM temp
+ WHERE hobby IS NOT NULL
+   AND hobby <> '낚시' 
