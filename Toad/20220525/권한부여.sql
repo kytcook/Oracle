5bd7d4f765af63f41a928111092f272e5afb52fa
kytcook <kytcook@naver.com>
2022-05-27 00:03:41 +0900

    backup_20220526

--- Toad/20220525/권한부여.sql
+++ Toad/20220525/권한부여.sql
@@ -1,16 +1,19 @@
 -- DCL - 권한부여
 
-grant create table to tomato with admin option 
+
+grant create session to KYTCOOK;
+
+grant create table to KYTCOOK with admin option;
 --테이블을 만드려는 권한을 토마토에게  준다.
 
-grant create view to tomato with admin option
+grant create view to KYTCOOK with admin option;
 --뷰를 만드는 권한을 준다
 
-alter user tomato quota unlimited on TS_TOMATO
+alter user KYTCOOK quota unlimited on TS_KYTCOOK;
 --테이블 스페이스에 대한 설정 
 
-grant create trigger to tomato
+grant create trigger to KYTCOOK;
 --트리거를 만드는 권한부여
 
-grant create sequence to tomato
+grant create sequence to KYTCOOK;
 -- 1일 증가하는 오브젝트 시퀀스?권한 부여
--- Toad/20220525/권한부여2.sql
+++ Toad/20220525/권한부여2.sql
@@ -1,34 +1,27 @@
-create tablespace ts_tomato
-datafile 'D:\oracle_App\oradata\orcl11\ts_tomato.dbf' size 100M;
+--sys계정에서 실행할 것?
+--
+--실습 시나리오
+--
+--오라클 서버에 tomato계정을 만들고 비번은 tiger로 한다.
+--tomato계정이 사용할 물리적인 공간을 만들어 준다.
+--ts_tomato라는 테이블 스페이스 이름으로 오라클이 설치되어 있는 물리적인 위치를 확인한 후
+--아래 DDL문을 sys계정에서 실행한다.
 
- 
+create tablespace ts_KYTCOOK
+datafile 'D:\app\kytco\oradata\orcl11\KYTCOOK.dbf' size 100M;
+
+
+--아래는 위에서 생성한 테이블스페이스가 정상적으로 만들어 졌는지 확인하는 DML문이다.
+--이것도 반드시 sys 계정에서 확인해야 한다.
 select tablespace_name, file_name, autoextensible, increment_by
       ,maxbytes
   from dba_data_files
- where tablespace_name = 'TS_TOMATO';
+ where tablespace_name = 'TS_KYTCOOK'
 
-create user tomato identified by tiger
-default tablespace TS_TOMATO;
+--tomato라는 계정을 만들고 비번은 tiger로 하며
+--tomato계정이 바라보는 테이블 스페이스는 ts_tomato로 한다.
+create user KYTCOOK identified by tiger
+default tablespace ts_KYTCOOK
 
-----------------------------------------------------------
 
-grant create session to tomato  with admin option;
 
--- DCL - 권한부여
-
-grant create table to tomato with admin option 
---테이블을 만드려는 권한을 토마토에게  준다.
-
-grant create view to tomato with admin option
---뷰를 만드는 권한을 준다
-
-alter user tomato quota unlimited on TS_TOMATO
---테이블 스페이스에 대한 설정 
-
-grant create trigger to tomato
---트리거를 만드는 권한부여
-
-grant create sequence to tomato
--- 1일 증가하는 오브젝트 시퀀스?권한 부여
-
-COMMIT;
