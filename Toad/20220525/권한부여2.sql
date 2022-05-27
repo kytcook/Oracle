168a9d35ddff08886900204ba01470382efc0643
DESKTOP-G09GV65\user1 <kytcook@naver.com>
2022-05-27 09:54:40 +0900

    ok

--- Toad/20220525/권한부여2.sql
+++ Toad/20220525/권한부여2.sql
@@ -10,6 +10,7 @@ select tablespace_name, file_name, autoextensible, increment_by
 create user tomato identified by tiger
 default tablespace TS_TOMATO;
 
+
 ----------------------------------------------------------
 
 grant create session to tomato  with admin option;
