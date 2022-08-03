6b3979b14b8c2a8b259542202f0d943f68fdff23
DESKTOP-G09GV65\user1 <kytcook@naver.com>
2022-08-03 15:01:34 +0900

    BOARD_220803

---
+++ 서블릿관련/220803_BOARD_OUTERJOIN.sql
@@ -0,0 +1,7 @@
+SELECT
+       bm.b_no, bm.b_title, bm.b_writer, bm.b_hit, bs.b_file, bm.b_date
+  FROM board_master_T bm LEFT OUTER JOIN board_sub_t bs
+    ON bm.b_no = bs.b_noBOARD_MASTER_T BOARD_MASTER_T 
+    
+    
+insert into 
