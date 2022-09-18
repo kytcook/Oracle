bf48bf645790c5d3c5ebdc1a1d27d9d721dbf4e9
kytcook <kytcook@naver.com>
2022-09-18 11:56:08 +0900

    DaoData_study_220918

---
+++ 서블릿관련/0917-transaction/dept쿼리문만들기.sql
@@ -0,0 +1,32 @@
+edit dept
+
+
+SELECT * FROM dept
+
+SELECT * FROM emp
+WHERE empno = 
+
+INSERT INTO dept(deptno,dname,loc) values(99,null,null)
+
+표준 JDBC로 처리 
+
+myBatis 에서는 널값이 들어오는 경우 문제를 삼는다.
+
+------------------------------------------------------------------
+
+emp
+
+dept
+
+SELECT deptno FROM dept
+MINUS
+SELECT deptno FROM emp
+
+delete from dept
+where deptno =:x
+
+select * from dept
+
+rollback
+
+------------------------------------------------------
