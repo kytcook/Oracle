a358606e67e6b2d19d8ba249b1afe4ad473c35d0
DESKTOP-C5S803A\kytcook <kytcook@naver.com>
2022-11-05 09:30:00 +0900

    syncAndClean_221105

--- 2차플젝 수정하기 쿼리.sql
+++
@@ -1,8 +0,0 @@
-UPDATE mkaddrtb
-   SET name    = '사과'
-      ,address = '인천광역시 부평구 계산동'
-      ,gender  = '0'
- WHERE id = 10;
- 
-SELECT * FROM MKADDRTB 
- WHERE id = 10;
--- New 1.sql
+++
@@ -1,9 +0,0 @@
-
-select mkaddrtb_id.nextval from dual;
-
-        INSERT INTO mkaddrtb(name, address, telephone, gender
-                           , relationship,birthday, comments, registedate, id)
-                     VALUES ('나신입, '서울시' '강남구 역삼동', '"02-555-888', '1'
-                           , 고교동창, '20001112', 개ㅣ인사업, to_char(sysdate.mkaddrtb_seq);, 
-                           
-        SElECT * from mkaddrtb
